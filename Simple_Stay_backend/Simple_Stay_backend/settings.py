0b2a900ddb4026db5bfdada1772866a22e4e4254
FASIL K <fasilmlm10@gmail.com>
2024-02-22 05:35:41 +0530

    added the cors new fromted link

--- Simple_Stay_backend/Simple_Stay_backend/settings.py
+++ Simple_Stay_backend/Simple_Stay_backend/settings.py
@@ -107,7 +107,8 @@ CORS_ALLOW_CREDENTIALS = True
 
 CORS_ALLOWED_ORIGINS = [
     'http://localhost:5173',
-    'http://127.0.0.1:5173',    
+    'http://127.0.0.1:5173',  
+    'https://simple-stay-fronted.vercel.app/',  
 ]
 
 
